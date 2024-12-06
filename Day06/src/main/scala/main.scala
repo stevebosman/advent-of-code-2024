f1beca1801de03bcb495223e3f903d554e30c914
Steve Bosman <stevebosman@gmail.com>
2024-12-06 23:02:01 +0000

    Day 6 part 2

--- Day06/src/main/scala/main.scala
+++ Day06/src/main/scala/main.scala
@@ -5,28 +5,35 @@ import scala.io.Source
 import scala.util.{Try, Using}
 
 @main
-def main(filename: String): Int =
+def main(filename: String): (Int, Int) =
   def readLines(filename: String): Try[List[String]] =
     Using(Source.fromFile(filename)) { source => source.getLines().toList }
 
   val map = readLines(filename).get
-  val start = map.zipWithIndex.filter {(s,i) => s.contains("^")}.map((s,i) => (s.indexOf("^"),i))(0)
-  val result = walk(map, start)
+  val start = map.zipWithIndex.filter { (s, i) => s.contains("^") }.map((s, i) => Position(s.indexOf("^"), i)).head
+  val uniquePositions = walk(map, DirectedPosition(start, North)).get.map(dp => dp.position)
+  val obstacles = uniquePositions.filter { p => walk(map, DirectedPosition(start, North), p).isEmpty }
+  val result = (uniquePositions.size, obstacles.size)
   println(result)
   result
 
 @tailrec
-def walk(map: List[String], position: (Int, Int), direction: Direction = North, positions: Set[(Int, Int)] = Set()): Int = {
+def walk(map: List[String], position: DirectedPosition, obstacle: Position = Position(-2, -2), positions: Set[DirectedPosition] = Set()): Option[Set[DirectedPosition]] = {
+  if (positions.contains(position)) return None
   val newPositions = positions + position
-  val forward = (position._1 + direction.dx, position._2 + direction.dy)
-  if (forward._2 < 0 || forward._1 < 0 || forward._2 >= map.length || forward._1 >= map.head.length) return positions.size + 1
-  val (newPosition, newDirection) = if (map(forward._2)(forward._1) == '#') {
-    (position, direction.turnRight)
+  val forward = Position(position.position.x + position.direction.dx, position.position.y + position.direction.dy)
+  if (forward.y < 0 || forward.y >= map.length || forward.x < 0 || forward.x >= map.head.length) return Some(newPositions)
+  val newPosition = if (forward == obstacle || map(forward.y)(forward.x) == '#') {
+    DirectedPosition(position.position, position.direction.turnRight)
   } else {
-    (forward, direction)
+    DirectedPosition(forward, position.direction)
   }
-  walk(map, newPosition, newDirection, newPositions)
+  walk(map, newPosition, obstacle, newPositions)
 }
+
+case class Position(x: Int, y: Int)
+
+case class DirectedPosition(position: Position, direction: Direction)
 
 enum Direction(val dx: Int, val dy: Int):
   def turnRight: Direction = Direction.fromOrdinal((this.ordinal + 1) % 4)
--- Day06/src/test/scala/MainTest.scala
+++ Day06/src/test/scala/MainTest.scala
@@ -2,8 +2,8 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class MainTest extends AnyFunSuite:
   test("Given straight ahead, part 1") {
-    assert(main("src/test/resources/straight.txt") === 2)
+    assert(main("src/test/resources/straight.txt") === (2, 0))
   }
   test("Given example, part 1 - strict") {
-    assert(main("src/test/resources/example1.txt") === 41)
+    assert(main("src/test/resources/example1.txt") === (41, 6))
   }
