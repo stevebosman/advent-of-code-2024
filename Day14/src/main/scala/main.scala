523d74e2b471ec90c2adca53eeceb8b6c96133e7
Steve Bosman <stevebosman@gmail.com>
2024-12-14 16:13:49 +0000

    Add visualisation in debug mode

--- Day14/src/main/scala/main.scala
+++ Day14/src/main/scala/main.scala
@@ -1,14 +1,21 @@
+import scala.Array.ofDim
 import scala.io.Source
 import scala.util.Using
 
 @main
-def main(iterations:Int, width:Int, height: Int, filename: String): Unit =
+def main(iterations:Int, width:Int, height: Int, filename: String, debug: Boolean = false): Unit =
   val robots = Using(Source.fromFile(filename)) {source => source.getLines().map(s=>Robot.of(s)).toList }.get
   (1 to iterations).foreach(i=> {
     robots.foreach(r => r.move(width, height))
+
+    if (debug) {
+      val myMatrix = ofDim[Int](height, width)
+      robots.foreach(r => myMatrix(r.p.y)(r.p.x) += 1)
+      println(myMatrix.map(l => l.map(i => if (i==0) {"."} else i.toString).mkString ).mkString("\n"))
+      println("")
+    }
   })
   val quadrants = robots.map(r=>r.p.quadrant(width/2, height/2))
   val safetyFactor = quadrants.filter(p=> p!=0).groupBy(identity).map(e => e._2.size).product
 
   println(s"${safetyFactor}")
-
